f4e1e721d6e625d0fb52f43a63811d71d3722b6a
서민혁 <mahns1201@gmail.com>
2021-10-24 00:47:04 +0900

    [del, add] init vue
    vue 프로젝트 초기화

--- front-end/v1/src/main.js
+++ front-end/v1/src/main.js
@@ -1,19 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import { createApp } from 'vue'
+import App from './App.vue'
 
-import { createApp } from "vue";
-import App from "./App";
-import router from "./router";
-
-Vue.config.productionTip = false;
-
-/* eslint-disable no-new */
-
-const app = createApp(App)
-  // .use(store)
-
-  .use(router);
-
-// globalComponents(app);
-// customComponents(app);
-app.mount("#app");
+createApp(App).mount('#app')
