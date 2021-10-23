96360e788e280d786e90308550d368952b965bf5
서민혁 <mahns1201@gmail.com>
2021-10-23 23:49:44 +0900

    [add] vue-router

--- front-end/v1/src/main.js
+++ front-end/v1/src/main.js
@@ -1,15 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+import { createApp } from "vue";
+import App from "./App";
+import router from "./router";
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+
+const app = createApp(App)
+  // .use(store)
+
+  .use(router);
+
+// globalComponents(app);
+// customComponents(app);
+app.mount("#app");
