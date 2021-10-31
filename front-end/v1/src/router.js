aa046e824ce9e48c4999c1fff9e51d460bfde7ba
서민혁 <mahns1201@gmail.com>
2021-10-31 23:30:36 +0900

    [feat] 404 page rendering

--- front-end/v1/src/router.js
+++ front-end/v1/src/router.js
@@ -1,12 +1,22 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "./views/Home.vue";
+import NotFound from "./views/NotFound.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/404",
+    name: "notFound",
+    component: NotFound,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: "/404",
   },
 ];
 
