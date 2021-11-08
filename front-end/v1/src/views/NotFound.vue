34bb1ef206e5bdcc5bdce5f813eeea514faaa325
서민혁 <mahns1201@gmail.com>
2021-11-08 23:34:04 +0900

    [feat] not-found page

--- front-end/v1/src/views/NotFound.vue
+++ front-end/v1/src/views/NotFound.vue
@@ -1,11 +1,59 @@
 <template>
-  <div>
-    <label>페이지를 찾을 수 없습니다.</label>
+  <div class="form">
+    <img src="../assets/sad.png" alt="../assets/logo.png" />
+    <div class="center">
+      <label class="title">404</label>
+      <label class="sub-title">The page you requested was not found</label>
+    </div>
+    <!-- <button class="btn" @click="onHome">Go Home</button> -->
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    onHome: function() {
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 90vh;
+}
+
+.center {
+  display: flex;
+  flex-direction: column;
+}
+
+.title {
+  font-size: xx-large;
+  font-weight: bolder;
+  text-align: center;
+  margin-top: 22px;
+}
+
+.sub-title {
+  font-size: x-large;
+  font-weight: lighter;
+  text-align: center;
+  margin-top: 2px;
+}
+
+.btn {
+  font-size: large;
+  background-color: black;
+  color: white;
+  border: none;
+  width: 15%;
+  height: 40px;
+  margin-top: 30px;
+}
+</style>
