d43b950cc5c7711bed79a74b31788cb0a499cab9
서민혁 <mahns1201@gmail.com>
2021-11-10 11:23:39 +0900

    [feat] AboutPage

---
+++ front-end/v1/src/views/AboutPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <h1>What's Ecolabs?</h1>
+    <p>
+      Lorem ipsum dolor sit amet, ad ius natum adolescens deseruisse. Sit at
+      sint vidisse diceret, vel facer numquam ei, idque ceteros voluptatibus ne
+      cum. Vel ipsum putent ut, cu mei discere vituperatoribus. Vis vivendum
+      gubergren referrentur ne, vim at platonem voluptatum.
+    </p>
+    <div class="ecolabs__targets">
+      <div class="target">
+        <div class="target__icon">
+          <img src="../assets/logo.png" alt="logo" />
+        </div>
+        <div class="target__title">What We Do</div>
+        <div class="target__description">
+          Usage, Common examples, Translation, Variants and technical
+          information
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
